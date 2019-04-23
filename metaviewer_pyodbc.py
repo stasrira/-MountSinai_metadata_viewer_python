8ba2ce8d2fbd8f64a12332cdfd8708b687685242
Stas Rirak <stasrira@yahoo.com>
2019-04-23 13:22:41 -0400

    Adds library file and utilization example file. metaviewer_pyodbc.py file adds functionality to retrieve metadata based on the comma delimited list of sample ids from Mount Sinai metadata database.

---
+++ metaviewer_pyodbc.py
@@ -0,0 +1,125 @@
+import pyodbc 
+import sys
+
+def printL (m):
+	if __name__ == '__main__':
+		print (m)
+
+class MetaViewer:
+	strConn_prod = 'Driver={SQL Server};Server=10.160.20.65\SSQL_2016;Database=dw_motrpac;User=mt_internal_user;Password=se@lf0n1nt3rn@l'
+	strConn_dev = 'Driver={SQL Server};Server=localhost\sqlexpress;Database=dw_motrpac;User=mt_internal_user;Password=se@lf0n1nt3rn@l'
+	strConn = ''
+	sqlproc_getMetadata = "usp_get_metadata_studies_combined_by_sids"
+	sqlproc_getMetaStudies = "select * from vw_get_study_stats"
+	#envir = ''
+	
+	def __init__(self, envir = "development", str_conn = ""):
+		self.envir = envir
+		
+		if len(str_conn) > 0:
+			self.strConn = str_conn
+		else:
+			if self.envir == "development":
+				self.strConn = self.strConn_dev
+			else:
+				self.strConn = self.strConn_prod
+		
+	def getCurConn (self):
+		return self.strConn
+	
+	def getCurrentEnvir (self):
+		return self.envir
+	
+	def setEnvir (self, envir = "development"):
+		self.envir = envir
+		if self.envir == "development":
+			self.strConn = self.strConn_dev
+		else:
+			self.strConn = self.strConn_prod
+	
+	def getMetadata (self, sample_ids):
+		#open connection and execute SQL query
+		conn = pyodbc.connect(self.strConn)
+		cursor = conn.cursor()
+		cursor.execute("exec " + self.sqlproc_getMetadata + "'" + sample_ids + "'")
+		
+		#returned recordsets
+		rs_out = []
+	
+		try:
+			#raise pyodbc.ProgrammingError("STAS123")
+			while True:
+				#print ("Loop---------")
+				rows = cursor.fetchall()
+				columns = [column[0] for column in cursor.description]
+				#printL (columns)
+				results = []
+				for row in rows:
+					results.append(dict(zip(columns, row)))
+				rs_out.append (results)
+				
+				cursor.nextset()
+
+		except Exception as e:
+
+			if hasattr(e, 'message'):
+				err_msg = e.message
+			else:
+				err_msg = e
+			
+			err_msg_template = "No results.  Previous SQL was not a query."
+			if str(err_msg) != err_msg_template:
+				raise Exception (e)
+				
+		return rs_out
+		
+	def getMetaStudies (self):
+		#open connection and execute SQL query
+		conn = pyodbc.connect(self.strConn)
+		cursor = conn.cursor()
+		cursor.execute(self.sqlproc_getMetaStudies)
+		
+		#returned recordset
+		rs_out = []
+		rows = cursor.fetchall()
+		columns = [column[0] for column in cursor.description]
+		#printL (columns)
+		results = []
+		for row in rows:
+			results.append(dict(zip(columns, row)))
+		rs_out.append (results)
+				
+		return rs_out
+
+#if executed by itself, do the following
+if __name__ == '__main__':
+
+	#utilization of the MetaViewer class
+	mv = MetaViewer ()
+
+	printL (">>>>>>>>>> Metadata >>>>>>>>>>>>>>>>")
+	#submit comma delimited list of sample_ids
+	#sid example: '99920015501, 99920015801,  80000885507,90001015204, 90001015502'
+	meta_out = mv.getMetadata ("99920015501, 99920015801")
+	#output results for test purposes
+	i = 0
+	for rs in meta_out:
+		printL ("=============Recordset " + str(i) + "==========")
+		for row in rs:
+			print (row)
+		i=i+1
+
+	printL (">>>>>>>>>> Studies >>>>>>>>>>>>>>>>")
+	studies = mv.getMetaStudies()
+	for rs in studies:
+		for row in rs:
+			print (row)
+		
+
+
+
+
+
+
+
+
---
+++ metaviewer_pyodbc_utilization.py
@@ -0,0 +1,33 @@
+import metaviewer_pyodbc as mvr 
+
+print ("-----------Start Utilization Example-----------")
+
+#utilization of the MetaViewer class
+mv = mvr.MetaViewer()
+
+print (">>>>>>>>>> Metadata >>>>>>>>>>>>>>>>")
+#submit comma delimited list of sample_ids
+#sid example: '99920015501, 99920015801,  80000885507,90001015204, 90001015502'
+meta_out = mv.getMetadata ("99920015501, 99920015801,  80000885507,90001015204, 90001015502")
+#output results for test purposes
+i = 0
+for rs in meta_out:
+	print ("=============Recordset " + str(i) + "==========")
+	for row in rs:
+		print (row)
+	i=i+1
+
+print (">>>>>>>>>> Studies >>>>>>>>>>>>>>>>")
+studies = mv.getMetaStudies()
+for rs in studies:
+	for row in rs:
+		print (row)
+		
+
+
+
+
+
+
+
+
